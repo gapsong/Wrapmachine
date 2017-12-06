f64028fdff6dc3a576a9611051871a67debbabb2
gap <khiemtonthat@yahoo.de>
2017-12-06 23:35:22 +0100

    fixed error

--- app/components/RhymeView.js
+++ app/components/RhymeView.js
@@ -6,11 +6,7 @@ class RhymeView extends React.Component {
         return (
             <div className="rhymeView">
                 {this.props.rhymes.map((item) => (
-<<<<<<< HEAD
                     <button>{item}</button>
-=======
-                    <span className="badge badge-pill badge-primary">{item}</span>
->>>>>>> 9ead40282c7083c9b27756e07fea9126355cd4e6
                 ))}
             </div>
         );
