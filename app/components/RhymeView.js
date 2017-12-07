a1a04a20dfb683e8649ae291ad69c26ae19da309
gap <khiemtonthat@yahoo.de>
2017-12-07 14:05:39 +0100

    test

--- app/components/RhymeView.js
+++ app/components/RhymeView.js
@@ -5,6 +5,7 @@ class RhymeView extends React.Component {
     render() {
         return (
             <div className="rhymeView">
+              test
                 {this.props.rhymes.map((item) => (
                     <p>{item}</p>
                 ))}
