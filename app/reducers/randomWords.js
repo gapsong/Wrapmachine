303d3005eefb240ab8a0ded87b7a7c4cb3e90b22
gap <khiemtonthat@yahoo.de>
2017-12-07 21:19:20 +0100

    added randomWords

---
+++ app/reducers/randomWords.js
@@ -0,0 +1,16 @@
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+export default function randomWords(state = {}, action) {
+  switch (action.type) {
+      case 'SHUFFLE':
+      return Object.assign([], shuffle(state));
+    default:
+      return state;
+  }
+}
