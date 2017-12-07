03742393776333ad75fc559a45fbad280c44a298
gap <khiemtonthat@yahoo.de>
2017-12-07 21:54:10 +0100

    deleted redux dev tool

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,53 +1,53 @@
-import {
-    applyMiddleware,
-    compose,
-    createStore
-} from 'redux'
-import thunk from 'redux-thunk'
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import rootReducer from '../reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-
-export default function configureStore(initialState) {
-    const logger = createLogger();
-    const store = createStore(
-        rootReducer,
-        initialState,
-        composeWithDevTools(applyMiddleware(thunk, promise, logger))
-    );
-
-    if (module.hot) {
-        module.hot.accept('../reducers/index', () => {
-            var nextRootReducer = require('../reducers/index').default;
-            store.replaceReducer(nextRootReducer);
-        });
-    }
-    return store;
-}
-//
-// import { applyMiddleware, compose, createStore } from 'redux';
-// import thunk from 'redux-thunk';
+// import {
+//     applyMiddleware,
+//     compose,
+//     createStore
+// } from 'redux'
+// import thunk from 'redux-thunk'
 // import promise from 'redux-promise';
 // import createLogger from 'redux-logger';
 // import rootReducer from '../reducers';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+//
 //
 // export default function configureStore(initialState) {
-//   const logger = createLogger();
-//   const store = createStore(
-//     rootReducer,
-//     initialState,
-//     applyMiddleware(thunk, promise, logger)
-//   );
+//     const logger = createLogger();
+//     const store = createStore(
+//         rootReducer,
+//         initialState,
+//         composeWithDevTools(applyMiddleware(thunk, promise, logger))
+//     );
 //
-//   if (module.hot) {
-//     // Enable hot module replacement for reducers
-//     module.hot.accept('../reducers', () => {
-//       const nextRootReducer = require('../reducers');
-//       store.replaceReducer(nextRootReducer);
-//     });
-//   }
-//
-//   return store;
+//     if (module.hot) {
+//         module.hot.accept('../reducers/index', () => {
+//             var nextRootReducer = require('../reducers/index').default;
+//             store.replaceReducer(nextRootReducer);
+//         });
+//     }
+//     return store;
 // }
+
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers'
+
+export default function configureStore(initialState) {
+  const logger = createLogger();
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk, promise, logger)
+  );
+
+  if (module.hot) {
+    // Enable hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+}
