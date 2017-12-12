93b6b24cf2121666fee4f552083c4ee813b568b7
gap <khiemtonthat@yahoo.de>
2017-12-12 21:14:48 +0100

    test

--- app/reducers/temp.js
+++ app/reducers/temp.js
@@ -2,7 +2,7 @@ export default function temp(state = {}, action) {
   switch (action.type) {
     case 'GET_RHYME_SUCCESSFUL':
     console.log("word",action.word);
-      return Object.assign([], action.word);
+      return [...action.word];
     default:
       return state;
   }
